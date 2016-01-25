3d3349a4b5fd5762864f2954119c45f517bf2733
brad pizzimenti and jared beckler <jared.beckler@gmail.com>
2016-01-25 08:37:23 -0800

    add first spec

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,8 @@
-describe('leapYear', function() {
-  it("is false for a year that is not divisible by 4, 100, or 400", function() {
-    expect(leapYear(1993)).to.equal(false);
+describe("Contact", function(){
+  it("creates a new contact with the given properties", function(){
+    var testContact = new Contact("Dave","Smith");
+    expect(testContact.firstName).to.equal("Dave");
+    expect(testContact.lastName).to.equal("Smith");
+    expect(testContact.addressData).to.eql([]);
   });
 });
